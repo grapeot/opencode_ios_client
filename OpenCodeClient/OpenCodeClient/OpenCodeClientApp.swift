bc9768178a01ce2e21c7f19aec93ceaf074b09cd
Yan Wang <grapeot@gmail.com>
2026-05-01 08:04:15 -0700

    feat: set visionOS default window size
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- OpenCodeClient/OpenCodeClient/OpenCodeClientApp.swift
+++ OpenCodeClient/OpenCodeClient/OpenCodeClientApp.swift
@@ -5,11 +5,21 @@
 
 import SwiftUI
 
+#if os(visionOS)
+private enum VisionWindowDefaults {
+    static let width: CGFloat = 2304
+    static let height: CGFloat = 1080
+}
+#endif
+
 @main
 struct OpenCodeClientApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        #if os(visionOS)
+        .defaultSize(width: VisionWindowDefaults.width, height: VisionWindowDefaults.height)
+        #endif
     }
 }
